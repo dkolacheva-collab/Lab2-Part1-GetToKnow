966c505c7146c1018eb7f8e5810ae3d18a948096
Daniela Kolacheva <dkolacheva@student.sdccd.edu>
2025-09-06 14:35:44 -0700

    Initial sharing of project

---
+++ GetToKnowMe.java
@@ -0,0 +1,16 @@
+//imports (ie import java.util.Scanner)
+
+/**
+ * This program will help to get to know me.
+ * @author Daniela Kolacheva, dkolacheva@student.sdccd.edu
+ * @version v1.0
+ * @since 9/6/2025
+ */
+public class GetToKnowMe
+{
+    public static void main (String[] args){
+        System.out.print("My name is Daniela Kolacheva.\n" );
+        System.out.print("My major is Computer Hardware Engineering.\n");
+        System.out.print("One of my favorite hobbies is to play a game with a good storyline.\n");
+    }
+}
